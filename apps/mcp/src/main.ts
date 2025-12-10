ab577f3b945593e9d4bcc011eb391308589f2f1e
Rafael González <rafael.gonzalez@nixs.com>
2025-12-10 16:20:50 +0100

    remove cors

--- apps/mcp/src/main.ts
+++ apps/mcp/src/main.ts
@@ -4,10 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT ?? 3030;
-  app.enableCors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
-  });
+  // app.enableCors({
+  //   origin: process.env.FRONTEND_URL,
+  //   credentials: true,
+  // });
   await app.listen(
     port,
     () => console.log(`MCP server is running on port ${port}`));
