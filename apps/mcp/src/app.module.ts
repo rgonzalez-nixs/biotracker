f7ec98203fb4ae203a94b0ed7d908288e7a40607
Rafael González <rafael.gonzalez@nixs.com>
2025-12-10 14:18:22 +0100

    add package to read .env

--- apps/mcp/src/app.module.ts
+++ apps/mcp/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { McpModule } from '@rekog/mcp-nest';
 import { McpTools } from './mcp-tools/mcp-tools.tools';
 import { McpToolsController } from './mcp-tools/mcp-tools.controller';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     McpModule.forRoot({
       name: 'biotracker-mcp',
       version: '0.0.1',
